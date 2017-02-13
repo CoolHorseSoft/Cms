940a161137864a3404df84f704da0d9617dea241
WeimaSoft <alexandersu@126.com>
2017-02-13 13:38:45 +0800

    Fix web project build nuger requirements

---
+++ Backup/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoolHorse.Cms.WebClient")]
+[assembly: AssemblyProduct("CoolHorse.Cms.WebClient")]
+[assembly: AssemblyCopyright("Copyright ©CoolHorse 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2c9cf073-e656-4822-92fe-5dcd32e479f8")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- CoolHorse.Cms.WebClient/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoolHorse.Cms.WebClient.MvcApplication" Language="C#" %>
