68a726d08e4eb1f65ec12f40df6b7831228d0b63
unknown <coolhorseatwork@126.com>
2017-02-16 21:23:57 +0800

    Add CmsAdmin

---
+++ CoolHorse.Cms.WebClient/CmsAdmin/scripts/admin.js
@@ -0,0 +1,8 @@
+﻿var app = angular.module('admin', []);
+
+app.run(["$rootScope", function ($rootScope) {
+    $rootScope.app = {
+        year : ((new Date()).getFullYear()),
+        name : "CoolHorse Soft Co.,Ltd"
+    }
+} ]);
