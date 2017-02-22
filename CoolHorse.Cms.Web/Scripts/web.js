65b9dfa131621f4543b4d0390fe9615fca53cb26
CoolHorseSoft <coolhorseatwork@126.com>
2017-02-22 23:57:43 +0800

    Web

---
+++ CoolHorse.Cms.Web/Scripts/web.js
@@ -0,0 +1,12 @@
+﻿//ui.router  Provide route services
+//ui.bootstrap Provide the collapse directive
+var app = angular.module('web', []);
+
+app.run(["$rootScope", function ($rootScope) {
+    $rootScope.app = {
+        layout: {
+            isBoxed: true,
+            isFixed: true
+        }
+    }
+} ]);
