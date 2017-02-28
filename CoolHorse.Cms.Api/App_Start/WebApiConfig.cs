de828d6ced40b4dd3461edb6031c1bcdaaa1d55f
CoolHorseSoft <coolhorseatwork@126.com>
2017-02-28 23:30:05 +0800

    Change Structure

--- CoolHorse.Cms.Api/App_Start/WebApiConfig.cs
+++ CoolHorse.Cms.Api/App_Start/WebApiConfig.cs
@@ -1,19 +1,24 @@
-﻿using System.Net.Http.Formatting;
-using System.Web.Http;
-
-namespace CoolHorse.Cms.Api
+﻿namespace CoolHorse.Cms.Api
 {
+    using System.Net.Http.Formatting;
+    using System.Web.Http;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
             var jsonFormatter = new JsonMediaTypeFormatter();
             config.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(jsonFormatter));
-
             
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApiAction",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
--- CoolHorse.Cms.Api/Global.asax.cs
+++ CoolHorse.Cms.Api/Global.asax.cs
@@ -1,10 +1,5 @@
 ﻿namespace CoolHorse.Cms.Api
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Net.Http;
-    using System.Net.Http.Formatting;
-    using System.Net.Http.Headers;
     using System.Web.Http;
 
     public class WebApiApplication : System.Web.HttpApplication
@@ -12,22 +7,6 @@
         protected void Application_Start()
         {
             WebApiConfig.Register(GlobalConfiguration.Configuration);
-        }
-    }
-
-    public class JsonContentNegotiator : IContentNegotiator
-    {
-        private readonly JsonMediaTypeFormatter _jsonFormatter;
-
-        public JsonContentNegotiator(JsonMediaTypeFormatter formatter)
-        {
-            _jsonFormatter = formatter;
-        }
-
-        public ContentNegotiationResult Negotiate(Type type, HttpRequestMessage request, IEnumerable<MediaTypeFormatter> formatters)
-        {
-            var result = new ContentNegotiationResult(_jsonFormatter, new MediaTypeHeaderValue("application/json"));
-            return result;
         }
     }
 }
