fb56f6ed6e344c1058b153e618a199306dad8be0
CoolHorseSoft <coolhorseatwork@126.com>
2017-03-08 21:19:11 +0800

    Validate with TinyMCE

--- CoolHorse.Cms.Api/CmsAdmin/scripts/admin.js
+++ CoolHorse.Cms.Api/CmsAdmin/scripts/admin.js
@@ -1029,8 +1029,11 @@ app.controller('NewsController', ['$scope', '$http', '$timeout', '$state', 'ngDi
 }]);
 
 app.controller('NewsDetailsController', ['$scope', '$http', '$state', '$stateParams', 'dataService', function ($scope, $http, $state, $stateParams, dataService) {
+    $scope.availableCategories = [];
     dataService.getResources('/api/category/GetAll', function (data) {
-        $scope.availableCategories = data.Response;
+        $scope.availableCategories.push({ Id: -1, Title: '-请选择类别-' });
+        $scope.availableCategories = $scope.availableCategories.concat(data.Response);
+        $scope.selectedCategory = -1;
     });
 
     $scope.model = $stateParams.model;
@@ -1038,10 +1041,12 @@ app.controller('NewsDetailsController', ['$scope', '$http', '$state', '$statePar
     $scope.cancel = function () { $state.go('news'); };
 
     $scope.save = function () {
+        tinymce.activeEditor.save();
+
         if (!$('#newsEditForm').valid()) {
             return false;
         }
-        $scope.model.Content = tinymce.get('txtContent').getContent();
+        $scope.model.Content = tinymce.activeEditor.getContent();
         if ($scope.model.Id <= 0) {
             $http.post('/api/news/create', $scope.model).success(function () { $state.go('news'); });
         } else {
@@ -1051,23 +1056,32 @@ app.controller('NewsDetailsController', ['$scope', '$http', '$state', '$statePar
 
     $.validator.addMethod('selectRequired',
         function (value, element, params) {
-            if (!$(element).selectedIndex) {
-                return false;
-            }
             if ($(element).val() < 0) {
                 return false;
             }
-
             return true;
         },
         "请选择类别");
 
+    $.validator.addMethod('editorRequired',
+        function (value, element, params) {
+            if ($(element).val() === "") {
+                return false;
+            }
+            return true;
+        },
+        "请输入文章内容或从模板中选择一个合适的模板进行编辑");
+
     $(document).ready(function(){
         $('#newsEditForm').validate({
             errorClass: 'text-danger',
+            ignore:[],
             rules: {
                 optCategory: {
                     selectRequired: true
+                },
+                txtContent: {
+                    editorRequired: true
                 }
             }
         });
