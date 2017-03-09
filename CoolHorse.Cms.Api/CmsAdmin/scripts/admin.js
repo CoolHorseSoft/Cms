134c44b0fa4cf484cb9e57e1e450e65bdccddd13
WeimaSoft <alexandersu@126.com>
2017-03-09 13:10:44 +0800

    Resolve

--- CoolHorse.Cms.Api/CmsAdmin/scripts/admin.js
+++ CoolHorse.Cms.Api/CmsAdmin/scripts/admin.js
@@ -1,170 +1,6 @@
-﻿(function ($, window, document) {
-
-    var containers = {},
-        messages = {},
-
-        notify = function (options) {
-
-            if ($.type(options) == 'string') {
-                options = { message: options };
-            }
-
-            if (arguments[1]) {
-                options = $.extend(options, $.type(arguments[1]) == 'string' ? { status: arguments[1] } : arguments[1]);
-            }
-
-            return (new Message(options)).show();
-        },
-        closeAll = function (group, instantly) {
-            if (group) {
-                for (var id in messages) { if (group === messages[id].group) messages[id].close(instantly); }
-            } else {
-                for (var id in messages) { messages[id].close(instantly); }
-            }
-        };
-
-    var Message = function (options) {
-
-        var $this = this;
-
-        this.options = $.extend({}, Message.defaults, options);
-
-        this.uuid = "ID" + (new Date().getTime()) + "RAND" + (Math.ceil(Math.random() * 100000));
-        this.element = $([
-            // @geedmo: alert-dismissable enables bs close icon
-            '<div class="uk-notify-message alert-dismissable">',
-                '<a class="close">&times;</a>',
-                '<div>' + this.options.message + '</div>',
-            '</div>'
-
-        ].join('')).data("notifyMessage", this);
-
-        // status
-        if (this.options.status) {
-            this.element.addClass('alert alert-' + this.options.status);
-            this.currentstatus = this.options.status;
-        }
-
-        this.group = this.options.group;
-
-        messages[this.uuid] = this;
-
-        if (!containers[this.options.pos]) {
-            containers[this.options.pos] = $('<div class="uk-notify uk-notify-' + this.options.pos + '"></div>').appendTo('body').on("click", ".uk-notify-message", function () {
-                $(this).data("notifyMessage").close();
-            });
-        }
-    };
-
-
-    $.extend(Message.prototype, {
-
-        uuid: false,
-        element: false,
-        timout: false,
-        currentstatus: "",
-        group: false,
-
-        show: function () {
-
-            if (this.element.is(":visible")) return;
-
-            var $this = this;
-
-            containers[this.options.pos].show().prepend(this.element);
-
-            var marginbottom = parseInt(this.element.css("margin-bottom"), 10);
-
-            this.element.css({ "opacity": 0, "margin-top": -1 * this.element.outerHeight(), "margin-bottom": 0 }).animate({ "opacity": 1, "margin-top": 0, "margin-bottom": marginbottom }, function () {
-
-                if ($this.options.timeout) {
-
-                    var closefn = function () { $this.close(); };
-
-                    $this.timeout = setTimeout(closefn, $this.options.timeout);
-
-                    $this.element.hover(
-                        function () { clearTimeout($this.timeout); },
-                        function () { $this.timeout = setTimeout(closefn, $this.options.timeout); }
-                    );
-                }
-
-            });
-
-            return this;
-        },
-
-        close: function (instantly) {
-
-            var $this = this,
-                finalize = function () {
-                    $this.element.remove();
-
-                    if (!containers[$this.options.pos].children().length) {
-                        containers[$this.options.pos].hide();
-                    }
-
-                    delete messages[$this.uuid];
-                };
-
-            if (this.timeout) clearTimeout(this.timeout);
-
-            if (instantly) {
-                finalize();
-            } else {
-                this.element.animate({ "opacity": 0, "margin-top": -1 * this.element.outerHeight(), "margin-bottom": 0 }, function () {
-                    finalize();
-                });
-            }
-        },
-
-        content: function (html) {
-
-            var container = this.element.find(">div");
-
-            if (!html) {
-                return container.html();
-            }
-
-            container.html(html);
-
-            return this;
-        },
-
-        status: function (status) {
-
-            if (!status) {
-                return this.currentstatus;
-            }
-
-            this.element.removeClass('alert alert-' + this.currentstatus).addClass('alert alert-' + status);
-
-            this.currentstatus = status;
-
-            return this;
-        }
-    });
-
-    Message.defaults = {
-        message: "",
-        status: "normal",
-        timeout: 5000,
-        group: null,
-        pos: 'top-center'
-    };
-
-
-    $["notify"] = notify;
-    $["notify"].message = Message;
-    $["notify"].closeAll = closeAll;
-
-    return notify;
-
-}(jQuery, window, document));
-
-//ui.router  Provide route services
+﻿//ui.router  Provide route services
 //ui.bootstrap Provide the collapse directive
-var app = angular.module('admin', ['ui.router','ui.bootstrap', 'ngDialog', 'ngGrid']);
+var app = angular.module('admin', ['ui.router', 'ui.bootstrap', 'ngDialog', 'ngGrid','oc.lazyLoad']);
 
 app.run(["$rootScope", "$state", '$templateCache', function ($rootScope, $state, $templateCache) {
     $rootScope.$state = $state;
@@ -207,8 +43,21 @@ app.provider('RouteHelpers', function () {
     };
 });
 
-app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', 'RouteHelpersProvider',
-function ($stateProvider, $locationProvider, $urlRouterProvider, helper) {
+app.config([
+    '$ocLazyLoadProvider', function ($ocLazyLoadProvider) {
+        'use strict';
+
+        // Lazy Load modules configuration
+        $ocLazyLoadProvider.config({
+            debug: false,
+            events: true
+        });
+
+    }
+]);
+
+app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', 'RouteHelpersProvider', '$ocLazyLoadProvider',
+function ($stateProvider, $locationProvider, $urlRouterProvider, helper, $ocLazyLoadProvider) {
     'use strict';
 
     // Set the following to true to enable the HTML5 Mode
@@ -227,6 +76,11 @@ function ($stateProvider, $locationProvider, $urlRouterProvider, helper) {
     }).state('category', {
         url: '/category',
         controller: 'CategoryController',
+        resolve: {
+            loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
+                return $ocLazyLoad.load("scripts/admin/categoryController.js");
+            }
+        ]},
         templateUrl: helper.basepath('category.htm')
     }).state('news', {
         url: '/news',
@@ -809,120 +663,6 @@ app.controller('SidebarController', ['$rootScope', '$scope', '$state', 'Utils',
 
   }]);
 
-app.controller('CategoryController', ['$scope', '$http', '$timeout', 'ngDialog', 'dataService', function ($scope, $http, $timeout, ngDialog, dataService) {
-    var getData = function () {
-        dataService.getResources('/api/category/GetAll', function (data) { $scope.myData = data.Response; });
-    }
-
-    var openDialog = function (templateId, data) {
-        ngDialog.open({
-            template: templateId,
-            className: 'ngdialog-theme-default',
-            scope: $scope,
-            data: data,
-            closeByEscape: false,
-            closeByDocument: false
-        });
-    }
-
-    var callBackForUpdate = function (data) {
-        if (data.ErrorMessage && data.ErrorMessage !== "") {
-            $.notify(data.ErrorMessage, { status: 'danger', pos: 'top-center',timeout:500 });
-            return false;
-        }
-
-        return true;
-    }
-
-    getData();
-
-    $scope.gridOption = {
-        data: 'myData',
-        rowHeight: 36,
-        headerRowHeight: 38,
-        multiSelect: false,
-        keepLastSelected: false,
-        showSelectionCheckbox: true,
-        selectedItems: [],
-        columnDefs: [
-            { field: 'Title', displayName: '标题' },
-            { field: 'Description', displayName: '类别描述' }
-        ]
-    };
-
-    $scope.openDialog = function (operationType) {
-        if (operationType === 1) {
-            var newItem = { Id: -1, Title: '', Description: '' };
-            openDialog('InserOrUpdate.html', newItem);
-            return;
-        }
-
-        if (operationType === 2) {
-            if ($scope.gridOption.selectedItems.length <= 0) {
-                return;
-            }
-            openDialog('InserOrUpdate.html', $scope.gridOption.selectedItems[0]);
-        }
-
-        if (operationType === 3) {
-            if ($scope.gridOption.selectedItems.length <= 0) {
-                return;
-            }
-            openDialog('DeleteConfirm.html', $scope.gridOption.selectedItems[0]);
-        }
-    }
-
-    $scope.saveFromDialog = function (data, operationType) {
-        if ($('#form').length > 0 && !$('#form').valid()) {
-            return false;
-        }
-        if ((operationType !== 3) && data) {
-            data.Id >= 0
-                ? dataService.updateResources('/api/category/update/', data, function (response) {
-                    if (callBackForUpdate(response)) {
-                        getData();
-                        ngDialog.closeAll();
-                    }
-                    return false;
-                })
-                : dataService.updateResources('/api/category/create/', data, function (response) {
-                    if (callBackForUpdate(response)) {
-                        getData();
-                        ngDialog.closeAll();
-                    }
-                    return false;
-                });
-        }
-
-        if (operationType === 3 && data) {
-            dataService.updateResources('/api/category/Delete/', data.Id, function () {
-                getData();
-                ngDialog.closeAll();
-            });
-        }
-    }
-
-    $scope.$on('ngDialog.opened', function (e, $dialog) {
-        if ($('#form').length > 0) {
-            $('#form').validate({
-                errorClass: 'text-danger',
-                rules: {
-                    txtCategoryTitle: {
-                        required: true,
-                        maxlength: 12
-                    }
-                },
-                messages: {
-                    txtCategoryTitle: {
-                        required: "请输入类别标题",
-                        maxlength: "标题长度不能大于12个字符"
-                    }
-                }
-            });
-        }
-    });
-}]);
-
 app.controller('NewsController', ['$scope', '$http', '$timeout', '$state', 'ngDialog', 'dataService', function ($scope, $http, $timeout, $state, dialog, dataService) {
     
 
@@ -950,7 +690,7 @@ app.controller('NewsController', ['$scope', '$http', '$timeout', '$state', 'ngDi
         columnDefs: [
             { field: 'Title', displayName: '标题' },
             { field: 'Category.Title', displayName: '类别' },
-            { field: 'DateUpdated', displayName: '最后更新' }
+            { field: 'DateUpdated', displayName: '最后更新', cellFilter: "date:'yyyy-MM-dd'" }
         ]
     };
 
---
+++ CoolHorse.Cms.Api/CmsAdmin/scripts/admin/categoryController.js
@@ -0,0 +1,113 @@
+﻿app.controller('CategoryController', ['$scope', '$http', '$timeout', 'ngDialog', 'dataService', function ($scope, $http, $timeout, ngDialog, dataService) {
+    var getData = function () {
+        dataService.getResources('/api/category/GetAll', function (data) { $scope.myData = data.Response; });
+    }
+
+    var openDialog = function (templateId, data) {
+        ngDialog.open({
+            template: templateId,
+            className: 'ngdialog-theme-default',
+            scope: $scope,
+            data: data,
+            closeByEscape: false,
+            closeByDocument: false
+        });
+    }
+
+    var callBackForUpdate = function (data) {
+        if (data.ErrorMessage && data.ErrorMessage !== "") {
+            $.notify(data.ErrorMessage, { status: 'danger', pos: 'top-center', timeout: 500 });
+            return false;
+        }
+
+        return true;
+    }
+
+    getData();
+
+    $scope.gridOption = {
+        data: 'myData',
+        rowHeight: 36,
+        headerRowHeight: 38,
+        multiSelect: false,
+        keepLastSelected: false,
+        showSelectionCheckbox: true,
+        selectedItems: [],
+        columnDefs: [
+            { field: 'Title', displayName: '标题' },
+            { field: 'Description', displayName: '类别描述' }
+        ]
+    };
+
+    $scope.openDialog = function (operationType) {
+        if (operationType === 1) {
+            var newItem = { Id: -1, Title: '', Description: '' };
+            openDialog('InserOrUpdate.html', newItem);
+            return;
+        }
+
+        if (operationType === 2) {
+            if ($scope.gridOption.selectedItems.length <= 0) {
+                return;
+            }
+            openDialog('InserOrUpdate.html', $scope.gridOption.selectedItems[0]);
+        }
+
+        if (operationType === 3) {
+            if ($scope.gridOption.selectedItems.length <= 0) {
+                return;
+            }
+            openDialog('DeleteConfirm.html', $scope.gridOption.selectedItems[0]);
+        }
+    }
+
+    $scope.saveFromDialog = function (data, operationType) {
+        if ($('#form').length > 0 && !$('#form').valid()) {
+            return false;
+        }
+        if ((operationType !== 3) && data) {
+            data.Id >= 0
+                ? dataService.updateResources('/api/category/update/', data, function (response) {
+                    if (callBackForUpdate(response)) {
+                        getData();
+                        ngDialog.closeAll();
+                    }
+                    return false;
+                })
+                : dataService.updateResources('/api/category/create/', data, function (response) {
+                    if (callBackForUpdate(response)) {
+                        getData();
+                        ngDialog.closeAll();
+                    }
+                    return false;
+                });
+        }
+
+        if (operationType === 3 && data) {
+            dataService.updateResources('/api/category/Delete/', data.Id, function () {
+                getData();
+                ngDialog.closeAll();
+            });
+        }
+    }
+
+    $scope.$on('ngDialog.opened', function (e, $dialog) {
+        if ($('#form').length > 0) {
+            $('#form').validate({
+                errorClass: 'text-danger',
+                rules: {
+                    txtCategoryTitle: {
+                        required: true,
+                        maxlength: 12
+                    }
+                },
+                messages: {
+                    txtCategoryTitle: {
+                        required: "请输入类别标题",
+                        maxlength: "标题长度不能大于12个字符"
+                    }
+                }
+            });
+        }
+    });
+}]);
